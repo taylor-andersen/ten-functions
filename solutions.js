b23c8ed994c25020db737ec149441fa349335f09
Taylor Andersen <tay.andersen99@gmail.com>
2023-10-17 14:52:08 -0500

    Completed ten-functions exercise. Oct17.

--- solutions.js
+++ solutions.js
@@ -21,3 +21,52 @@
  * concat
  */
 
+// 1. Define a function named isTrue
+function isTrue(input) {
+    return input === true;
+}
+
+// 2. Define a function named isFalse
+function isFalse(input) {
+    return input === false;
+}
+
+// 3. Define a function named 'not' that returns the boolean opposite of input
+function not(input) {
+    return !input === true;
+}
+
+// 4. Define a function named 'addOne' that takes number and returns value plus 1.
+function addOne(num) {
+    return parseFloat(num) + 1;
+}
+
+// 5. Define a function named 'isEven' and returns true if even
+function isEven(num) {
+    return num % 2 === 0
+}
+
+// 6. Define a function named 'isIdentical'
+function isIdentical(input, input2) {
+    return input === input2;
+}
+
+// 7. Define a function named 'isEqual'
+function isEqual(input, input2) {
+    return input == input2;
+}
+
+// 8. Define a function name 'or'
+function or(input, input2) {
+    return input || input2;
+}
+
+// 9. Define a function named 'and'
+function and(input, input2) {
+    return input && input2;
+}
+
+// 10. Define a function named concat
+function concat(input, input2) {
+    return `${input}` + `${input2}`;
+}
